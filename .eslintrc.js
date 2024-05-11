00fe4502b30142766a7882c34b1a1482a2bdd852
Ivan Royo <118160820+ivanbisimbrero@users.noreply.github.com>
2024-05-11 18:24:22 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,35 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
-  },
-  "extends": "eslint:recommended",
-  "rules": {
-    "no-cond-assign": [
-        "error",
-        "always"
-    ],
-    "indent": [
-        "error",
-        2
-    ],
-    "no-use-before-define": "error",
-    "semi": [
-        "error", 
-        "always"
-    ],
-    "no-trailing-spaces": "error",
-    "prefer-const": "error"
-  }
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-cond-assign": [
+            "error",
+            "always"
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "no-unused-vars": "off",
+        "semi": [
+            "error",
+            "always"
+        ]
+        "no-trailing-spaces": "error",
+        "prefer-const": "error"
+    }
 };
